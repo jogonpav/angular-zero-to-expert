2bef525827f985641abad869bca0321c4382f41e
jogonpav <josepabonvera@gmail.com>
2022-10-14 23:33:51 -0500

    "added comunication between component Input - Ouput"

---
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegueta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  agregarNuevoPersonaje(character: Personaje) {
+    this.personajes.push( character )
+  }
+
+
+
+}
