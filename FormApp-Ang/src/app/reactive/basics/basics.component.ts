43cfd269f8d830a7e905a35a952485b2748b5788
jogonpav <josepabonvera@gmail.com>
2023-04-26 00:10:43 -0500

    "Validations added and error messages implemented"

--- FormApp-Ang/src/app/reactive/basics/basics.component.ts
+++ FormApp-Ang/src/app/reactive/basics/basics.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basics.component.html'
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
 
 /*   myForm: FormGroup = new FormGroup(
     { 
@@ -16,13 +16,36 @@ export class BasicsComponent {
   ) */
 
   myForm: FormGroup = this.formBuilder.group({
-    name: [ 'RTX 4080ti', [Validators.required, Validators.minLength(3)]],
-    price: [1500,[Validators.required, Validators.minLength(0)]],
-    stocks: [5, [Validators.required, Validators.minLength(0)]],
+    name: [ , [Validators.required, Validators.minLength(3)]],
+    price: [ ,[Validators.required, Validators.minLength(0)]],
+    stocks: [ , [Validators.required, Validators.minLength(0)]],
   })
 
   constructor( private formBuilder: FormBuilder) { }
 
+  ngOnInit(): void {
+      this.myForm.reset(
+        {
+          name: 'RTX 4080ti',
+          price: 1600
+        }
+      )
+  }
 
+  save() {
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
+
+  notFieldValid(field: string){
+    return this.myForm.controls[field].errors 
+            && this.myForm.controls[field].touched
+  }
 
 }
