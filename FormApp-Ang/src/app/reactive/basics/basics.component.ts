3544e473c549c46fdf59e631fa0dee392d603fc7
jogonpav <josepabonvera@gmail.com>
2023-04-25 22:06:17 -0500

    "Added validations to form"

--- FormApp-Ang/src/app/reactive/basics/basics.component.ts
+++ FormApp-Ang/src/app/reactive/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -16,9 +16,9 @@ export class BasicsComponent {
   ) */
 
   myForm: FormGroup = this.formBuilder.group({
-    nombre: [ 'RTX 4080ti'],
-    price: [1500],
-    stocks: [5],
+    name: [ 'RTX 4080ti', [Validators.required, Validators.minLength(3)]],
+    price: [1500,[Validators.required, Validators.minLength(0)]],
+    stocks: [5, [Validators.required, Validators.minLength(0)]],
   })
 
   constructor( private formBuilder: FormBuilder) { }
