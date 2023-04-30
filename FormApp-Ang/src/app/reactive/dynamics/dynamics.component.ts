36e351a40b296298dc42e079be66005a7eb936b6
jogonpav <josepabonvera@gmail.com>
2023-04-29 22:47:15 -0500

    "FormArray Implemented in game favorite new"

--- FormApp-Ang/src/app/reactive/dynamics/dynamics.component.ts
+++ FormApp-Ang/src/app/reactive/dynamics/dynamics.component.ts
@@ -1,38 +1,53 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html'
 })
 export class DynamicsComponent {
-  
+
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
-    favourites: this.fb.array( [
+    favourites: this.fb.array([
       this.fb.control('Metal Gear', Validators.required), //this is a longer way 
       ['Death Stranding', Validators.required] // this is a short way 
-    ] , Validators.required)
+    ], Validators.required)
   })
 
+  //favouriteNew: FormControl = new FormControl('', Validators.required);
+  favouriteNew: FormControl = this.fb.control('', Validators.required)
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
-  get favouriteArr (){
+  get favouriteArr() {
     //return this.myForm.controls['favourites'] as FormArray
     return this.myForm.get('favourites') as FormArray;
   }
 
-  save(){ 
-    if(this.myForm.invalid){
+  addFavourite() {
+    if (this.favouriteNew.invalid) { return }
+
+    //((this.myForm.controls['favourites']) as FormArray).push(new FormControl(this.favouriteNew.value))
+
+    //this.favouriteArr.push(this.favouriteNew); //this form is not correct beacause JS uses variables by reference
+    //this.favouriteArr.push(new FormControl(this.favouriteNew.value, Validators.required)); //It's necessary to declare the form control variable again
+    this.favouriteArr.push(this.fb.control(this.favouriteNew.value, Validators.required)) //this form using FormBuilder
+
+    this.favouriteNew.reset();
+
+  }
+
+  save() {
+    if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return
     }
   }
 
-  notFieldValid(field: string){
-    return this.myForm.controls[field].errors 
-            && this.myForm.controls[field].touched
+  notFieldValid(field: string) {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
   }
 
 }
