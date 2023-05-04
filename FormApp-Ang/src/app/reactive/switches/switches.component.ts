43981438abfdbde2f42374076c98ef7ee82e07d9
jogonpav <josepabonvera@gmail.com>
2023-05-04 00:08:36 -0500

    "Added switches in reactive form"

--- FormApp-Ang/src/app/reactive/switches/switches.component.ts
+++ FormApp-Ang/src/app/reactive/switches/switches.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    notifications: [true, Validators.required],
+    terms: [true, Validators.requiredTrue]
+
+  })
+  person = {
+    gender: 'F',
+    notifications: true,
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset({ ...this.person, terms: true })
+  }
+
+  save(){
+    const formValue = {... this.myForm.value}
+    delete formValue.terms;
+    this.person = formValue;
+  }
 
 }
