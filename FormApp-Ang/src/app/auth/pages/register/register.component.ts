c527b6362c6955ea609125d6c9c60b056e8e9012
jogonpav <josepabonvera@gmail.com>
2023-05-18 22:36:27 -0500

    "Async validation for email, confirm if email exist"

--- FormApp-Ang/src/app/auth/pages/register/register.component.ts
+++ FormApp-Ang/src/app/auth/pages/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 
 import { cantBeStrider, emailPattern, nameLastNamePatter } from 'src/app/shared/validator/validations'; //Validators whome no requires services to be implmented
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,7 @@ export class RegisterComponent implements OnInit {
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.vs.nameLastNamePatter)]],
-    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.vs.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
@@ -24,7 +25,9 @@ export class RegisterComponent implements OnInit {
 
   )
 
-  constructor(private fb: FormBuilder, private vs: ValidatorService) { }
+  constructor( private fb: FormBuilder, 
+               private vs: ValidatorService,
+               private  emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
     this.myForm.reset({
